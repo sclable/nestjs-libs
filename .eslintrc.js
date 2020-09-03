e401f354b9a824bcb2a568f1513266d7c8b10a81
Norbert Lehotzky <norbert.lehotzky@sclable.com>
2020-09-03 11:47:25 +0200

    chore(*): add tsconfig & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['./node_modules/@sclable/eslint-config/index.js'],
+  root: true,
+  ignorePatterns: ['!.eslintrc.js', 'node_modules', 'dist'],
+  rules: {
+    // override or extend rules coming from `@sclable/lint` as needed
+  },
+}
